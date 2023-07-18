d4ab0032eed85a7a1a7aee76ce6ec2dd8224d16d
AdityaKandel <addrido@gmail.com>
2023-07-17 20:57:55 -0400

    added dependices for hosting

--- homeprice/.next/static/chunks/webpack.js
+++ homeprice/.next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "b3fe99357adf3598"; }
+/******/ 		__webpack_require__.h = function() { return "c5545b14346b7585"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
